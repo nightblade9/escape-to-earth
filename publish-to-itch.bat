f1e55bb336eff774929aeb3d2ca4b52693ff3a05
nightblade9 <nightbladecodes@gmail.com>
2018-08-03 23:25:47 -0400

    Add script to publish to Itch via Butler!

---
+++ publish-to-itch.bat
@@ -0,0 +1,5 @@
+cd EscapeToEarth\bin\Release\netcoreapp2.1\win10-x64
+butler push publish deengames/escape-to-earth:windows
+cd ..
+cd ubuntu.16.10-x64
+butler push publish deengames/escape-to-earth:linux
