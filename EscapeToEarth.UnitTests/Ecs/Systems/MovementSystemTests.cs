e349e8bd5bb72421364f75fce8982e4be4b57074
nightblade9 <nightbladecodes@gmail.com>
2018-08-13 12:55:50 -0400

    Add movement system stairs down traversal tests Clean up duplicate task in `tasks.json`

--- EscapeToEarth.UnitTests/Ecs/Systems/MovementSystemTests.cs
+++ EscapeToEarth.UnitTests/Ecs/Systems/MovementSystemTests.cs
@@ -61,5 +61,127 @@ namespace EscapeToEarth.UnitTest.Ecs.Systems
             Assert.That(player.Position.X, Is.EqualTo(expectedX)); // Position changed
             Assert.That(playerMoved, Is.EqualTo(isDestinationWalkable)); // Event fired (or not)
         }
+
+        [TestCase(Keys.LeftShift)]
+        [TestCase(Keys.RightShift)]
+        public void UpdateMovesPlayerToNextFloorIfPlayerStandsAboveStairsDownAndShiftDotKeysAreDown(Keys shiftKey)
+        {
+            // Arrange
+            const int StairsX = 3;
+            const int StairsY = 3;
+
+            var player = new Entity();
+            player.Set(new MoveToKeyboardComponent(player));
+            var system = new MovementSystem(player);
+
+            var map = new ArrayMap<AbstractMapTile>(5, 5);
+            for (var y = 0; y < map.Height; y++)
+            {
+                for (var x = 0; x < map.Width; x++)
+                {
+                    map[x, y] = new FloorTile();
+                }
+            }
+
+            map[StairsX, StairsY] = new StairsDownTile();
+            EventBus.Instance.Broadcast("Map changed", map);
+
+            player.Position.X = StairsX;
+            player.Position.Y = StairsY;
+
+            SadConsole.Global.KeyboardState.KeysDown.Add(AsciiKey.Get(shiftKey));
+            SadConsole.Global.KeyboardState.KeysDown.Add(AsciiKey.Get(Keys.OemPeriod));
+            
+            bool usedStairs = false;
+            EventBus.Instance.Register<string>("Player used stairs", (floorNumber) => usedStairs = true);
+            
+            // Act
+            system.Update(0);
+
+            // Assert
+            Assert.That(usedStairs, Is.True);
+        }
+
+        [TestCase(Keys.LeftShift, 1, 0)]
+        [TestCase(Keys.RightShift, 0, -1)]
+        public void UpdateDoesNotMovePlayerToNextFloorIfPositionIsWrong(Keys shiftKey, int xOffBy, int yOffBy)
+        {
+            // Arrange
+            const int StairsX = 3;
+            const int StairsY = 3;
+
+            var player = new Entity();
+            player.Set(new MoveToKeyboardComponent(player));
+            var system = new MovementSystem(player);
+
+            var map = new ArrayMap<AbstractMapTile>(5, 5);
+            for (var y = 0; y < map.Height; y++)
+            {
+                for (var x = 0; x < map.Width; x++)
+                {
+                    map[x, y] = new FloorTile();
+                }
+            }
+
+            map[StairsX, StairsY] = new StairsDownTile();
+            EventBus.Instance.Broadcast("Map changed", map);
+
+            player.Position.X = StairsX + xOffBy;
+            player.Position.Y = StairsY + yOffBy;
+
+            SadConsole.Global.KeyboardState.KeysDown.Add(AsciiKey.Get(shiftKey));
+            SadConsole.Global.KeyboardState.KeysDown.Add(AsciiKey.Get(Keys.OemPeriod));
+            
+            bool usedStairs = false;
+            EventBus.Instance.Register<string>("Player used stairs", (floorNumber) => usedStairs = true);
+            
+            // Act
+            system.Update(0);
+
+            // Assert
+            Assert.That(usedStairs, Is.False);
+        }
+
+        [TestCase(Keys.LeftShift,  Keys.Delete)]
+        [TestCase(Keys.RightShift,  Keys.K)]
+        public void UpdateDoesNotMovePlayerToNextFloorIfKeyCombinationDownIsWrong(Keys shiftKey, Keys otherKey)
+        {
+            // Arrange
+            const int StairsX = 3;
+            const int StairsY = 3;
+
+            var player = new Entity();
+            player.Set(new MoveToKeyboardComponent(player));
+            var system = new MovementSystem(player);
+
+            var map = new ArrayMap<AbstractMapTile>(5, 5);
+            for (var y = 0; y < map.Height; y++)
+            {
+                for (var x = 0; x < map.Width; x++)
+                {
+                    map[x, y] = new FloorTile();
+                }
+            }
+
+            map[StairsX, StairsY] = new StairsDownTile();
+            EventBus.Instance.Broadcast("Map changed", map);
+
+            player.Position.X = StairsX;
+            player.Position.Y = StairsY;
+
+            SadConsole.Global.KeyboardState.KeysDown.Add(AsciiKey.Get(shiftKey));
+            SadConsole.Global.KeyboardState.KeysDown.Add(AsciiKey.Get(otherKey));
+            
+            bool usedStairs = false;
+            EventBus.Instance.Register<string>("Player used stairs", (floorNumber) => usedStairs = true);
+            
+            // Act
+            system.Update(0);
+
+            // Assert
+            Assert.That(usedStairs, Is.False);
+        }
+
+        
     }
 }
