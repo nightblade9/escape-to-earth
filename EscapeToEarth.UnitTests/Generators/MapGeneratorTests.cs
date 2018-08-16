ef7cf0ce4eb9906c62f3460de0fed9771389ac41
nightblade9 <nightbladecodes@gmail.com>
2018-08-16 13:19:10 -0400

    Fix flakey test once and for all; thanks, A*!

--- EscapeToEarth.UnitTests/Generators/MapGeneratorTests.cs
+++ EscapeToEarth.UnitTests/Generators/MapGeneratorTests.cs
@@ -3,6 +3,7 @@ using EscapeToEarth.Generators;
 using EscapeToEarth.UnitTest.Helpers;
 using GoRogue;
 using GoRogue.MapViews;
+using GoRogue.Pathing;
 using NUnit.Framework;
 using System.Collections.Generic;
 using System;
@@ -14,16 +15,18 @@ namespace EscapeToEarth.UnitTest.Generators
     public class MapGeneratorTests
     {
         [Test]
-        // This test occassionally takes >= 10 seconds. That's okay. It's not flaky this way.
         public void GenerateFloorGeneratesMapWithWalkablePathFromPlayerToStairsAndStairsIsSufficientlyFarAway()
         {
-            // Generate map
+            ////
+            // With small map sizes (eg. 15x15), GoRogue often generates solid maps or maps with very few walkable tiles.
+            // See: https://github.com/Chris3606/GoRogue/issues/56
+            // Generate map big enough to separate player/stairs. Use A* path-finding to quickly walk it.
+            ////
+
             // Get the player and stairs position
             // Path-find (BFS) and make sure we can reach
             // verify the distance is large enough
-
-            // CRITICAL: make sure the map is big enough to position the player far from stairs.
-            var mapData = MapGenerator.GenerateFloor(1, 20, 20);
+            var mapData = MapGenerator.GenerateFloor(1, 40, 40);
 
             var map = mapData.Map;
             var playerPosition = mapData.PlayerPosition;
@@ -33,39 +36,19 @@ namespace EscapeToEarth.UnitTest.Generators
             Assert.That(map[playerPosition.X, playerPosition.Y], Is.True);
             Assert.That(map[stairsDownPosition.X, stairsDownPosition.Y], Is.True);
 
-            // Path-find! This could take a while ...
-            var positionsToExplore = new List<Coord>();
-            positionsToExplore.Add(playerPosition);
-
-            var exploredPositions = new List<Coord>();
-
-            Coord currentPosition = null;
-            DateTime start = DateTime.Now;
-
-            while (positionsToExplore.Any() && currentPosition != stairsDownPosition)
+            // Path-find! Use A* path-finding because breadth-first search seems to take forever.
+            var pathFinder = new AStar(map, Distance.MANHATTAN); // Manhattan = four-way only
+            var path = pathFinder.ShortestPath(playerPosition, stairsDownPosition);
+            Assert.That(path, Is.Not.Null);
+            
+            foreach (var step in path.Steps)
             {
-                currentPosition = positionsToExplore.ElementAt(0);
-                positionsToExplore.Remove(currentPosition);
-                exploredPositions.Add(currentPosition);
-
-                this.AddIfWalkableAndNeverChecked(map, currentPosition.X - 1, currentPosition.Y, exploredPositions, positionsToExplore);
-                this.AddIfWalkableAndNeverChecked(map, currentPosition.X + 1, currentPosition.Y, exploredPositions, positionsToExplore);
-                this.AddIfWalkableAndNeverChecked(map, currentPosition.X, currentPosition.Y - 1, exploredPositions, positionsToExplore);
-                this.AddIfWalkableAndNeverChecked(map, currentPosition.X, currentPosition.Y + 1, exploredPositions, positionsToExplore);
+                // Make sure it gave us a walkable path. Sorry, I don't understand the GoRogue docs on ShortestPath.
+                Assert.That(map[step.X, step.Y], Is.EqualTo(true));
             }
-
-            Assert.That(currentPosition, Is.EqualTo(stairsDownPosition), "Couldn't find a way from current position to stairs");
 
             var playerToStairsDistance = Math.Abs(playerPosition.X - stairsDownPosition.X) + Math.Abs(playerPosition.Y - stairsDownPosition.Y);
             Assert.That(playerToStairsDistance, Is.GreaterThanOrEqualTo(MapGenerator.MinimumPlayerStairsDistance));
-        }
-
-        public void AddIfWalkableAndNeverChecked(ArrayMap<bool> map, int x, int y,  List<Coord> exploredPositions,  List<Coord> positionsToExplore)
-        {
-            if (x >= 0 && x < map.Width && y >= 0 && y < map.Height && map[x, y] == true && !exploredPositions.Any(c => c.X == x && c.Y == y))
-            {
-                positionsToExplore.Add(Coord.Get(x, y));
-            }
         }
     }
 }
